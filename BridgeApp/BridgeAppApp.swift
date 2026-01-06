84a83ce41f8b035a730a227118e4736323d81bb0
bree-jeune <bree.j.32@gmail.com>
2026-01-06 14:10:08 -0800

    Initial Commit

---
+++ BridgeApp/BridgeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BridgeAppApp.swift
+//  BridgeApp
+//
+//  Created by Bree Jeune on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct BridgeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
